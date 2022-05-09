a55afaf58694868228aa960c94e3a389a0ef8fb2
luccasl <luccasleite@hotmail.com>
2022-05-08 22:50:35 -0300

    feat(button): add hover animation
    Add hover animation to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,10 +8,18 @@ const Button = styled.button<{
   height: 3rem;
   border: ${props => props.secondary ? `1px solid` : 'none'};
   border-color: ${props => props.secondary ? props.theme.colors.primary : 'transparent'};
-  box-shadow: 0 .1rem .2rem rgba(0, 0, 0, .2);
+  box-shadow: 0 .1rem .2rem ${props => props.theme.colors.shadow};
   color: ${props => props.secondary ? props.theme.colors.primary : props.theme.colors.light};
   font-size: 1rem;
   font-weight: semi-bold;
+
+  transition-property: box-shadow;
+  transition-duration: 200ms;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 .2rem .8rem ${props => props.theme.colors.shadow};
+  }
 `
 
 export { Button }
