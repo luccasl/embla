ccadbd6266c8316d395ec207fbce37f372ccdcf6
luccasl <luccasleite@hotmail.com>
2022-05-14 23:56:36 -0300

    refactor(datatableheader): remove unused button
    Remove unused button from DataTable's header

--- src/components/DataTable/DataTableHeader.tsx
+++ src/components/DataTable/DataTableHeader.tsx
@@ -1,22 +1,17 @@
-import { MdFilterAlt } from "react-icons/md"
 import styled from "styled-components"
-import { HeaderButton } from "./HeaderButton"
 import { SearchBar } from "./SearchBar"
 
 const DataTableHeaderContainer = styled.div`
     height: 3rem;
     border-bottom: 1px solid #dfdfdf;
     display: flex;
-    padding: 0.8rem 0.25rem;
+    padding: 0.8rem 1rem;
     align-items: center;
 `
 
 const DataTableHeader: React.FC = () => {
     return (
         <DataTableHeaderContainer>
-            <HeaderButton>
-                <MdFilterAlt size={21} />
-            </HeaderButton>
             <SearchBar />
         </DataTableHeaderContainer>
     )
